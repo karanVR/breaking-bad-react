26a1e6a43c3475e71b85068f1c402404cdc08626
karanVR <karanveerraghuvanshi@gmail.com>
2021-11-02 19:49:36 +0530

    Breaking BadAPI
    BreakingBadReact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Header from "./components/ui/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
+import Search from "./components/ui/Search";
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
+  //Quert State is made taaki jab search bar mein 
+  //kuch enter karein to us condition ke base pe Filter kar sakein..
+  //Query ko Search mein as a prop pass karke wahan set kar denge
+  //aur use yahan Fetch karte time kar sakte hain
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      );
+
+      console.log(result.data);
+
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, [query]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Search getQuery={(q) => setQuery(q)}/>
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
---
+++ src/components/characters/CharacterGrid.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CharacterItem from './CharacterItem'
+import Spinner from '../ui/Spinner'
+
+const CharacterGrid = ( {items, isLoading} ) => {
+    return isLoading ? <Spinner /> : 
+    <section className='cards'>
+        {items.map(item => (
+            // <h1 key={item.char_id}>{item.name}</h1>
+            <CharacterItem  key={item.char_id} item={item} />
+        ))}
+    </section>
+}
+
+export default CharacterGrid
